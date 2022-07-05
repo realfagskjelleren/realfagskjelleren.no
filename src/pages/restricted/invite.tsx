abf66c0097930902976c8f7533a55b4b59d35cb4
EvenAsl <85791946+EvenAsl@users.noreply.github.com>
2022-07-05 17:38:38 +0200

    Invite user via page, query of invited users

--- src/pages/restricted/invite.tsx
+++ src/pages/restricted/invite.tsx
@@ -1,16 +1,28 @@
+import { InferQueryOutput, trpc } from "@/utils/trpc";
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
 import React from "react";
 
 const Invite: NextPage = () => {
+	const router = useRouter();
+	const invited = trpc.useQuery(["invite.all"]);
+	const invite = trpc.useMutation(["invite.invite"]);
+	const dataLoaded = invited.data && !invited.isLoading;
+
 	return (
 		<div className="h-screen flex flex-col justify-center">
 			<div className="text-2xl flex flex-row justify-center">Invite User</div>
 			<div className="p-2" />
 			<Formik
 				initialValues={{ email: "" }}
-				onSubmit={(values) => {
-					console.log(values);
+				onSubmit={(values, { setErrors }) => {
+					invite.mutate({ email: values.email });
+					if (!invite.isSuccess && invite.isError) {
+						setErrors({ email: invite.error.message });
+					} else if (invite.isSuccess) {
+						router.push("/restricted/dashboard");
+					}
 				}}
 			>
 				{() => (
@@ -40,8 +52,39 @@ const Invite: NextPage = () => {
 					</div>
 				)}
 			</Formik>
+			<div className="p-5" />
+			{dataLoaded && <InvitedUsers users={invited.data} />}
 		</div>
 	);
 };
 
 export default Invite;
+
+const InvitedUsers: React.FC<{ users: InferQueryOutput<"invite.all"> }> = (
+	props
+) => {
+	return (
+		<div className="overflow-x-auto w-full flex flex-col items-center">
+			<div className="text-2xl flex flex-row justify-center">Invited Users</div>
+			<div className="p-2" />
+			<table className="table w-3/4">
+				<thead>
+					<tr>
+						<th></th>
+						<th>Email</th>
+						<th>Date invited</th>
+					</tr>
+				</thead>
+				<tbody>
+					{props.users.map((user, index) => (
+						<tr key={index}>
+							<td>{index + 1}</td>
+							<td>{user.email}</td>
+							<td>{user.invitedAt.toDateString()}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+};
