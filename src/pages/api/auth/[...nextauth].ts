38a3c48aae611f52e91263e64456fca10a35461f
EvenAsl <85791946+EvenAsl@users.noreply.github.com>
2022-07-05 16:21:16 +0200

    Restrict sign in to invited or existing users

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -14,6 +14,28 @@ export const authOptions: NextAuthOptions = {
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
 		}),
 	],
+	callbacks: {
+		async signIn({ profile }) {
+			// Already exists => invited before.
+			const alreadyExists = await prisma.user.findUnique({
+				where: { email: profile.email },
+			});
+			if (alreadyExists) return true;
+
+			// Invited => allowed to sign in
+			const isInvited = await prisma.invitedUser.findUnique({
+				where: { email: profile.email },
+			});
+			if (isInvited) {
+				// Remove invite since use will exist after sign in
+				await prisma.invitedUser.delete({ where: { email: profile.email } });
+				return true;
+			}
+
+			// If not existing or invited, no sign in
+			return false;
+		},
+	},
 };
 
 export default NextAuth(authOptions);
