1b125b8b2dc5f613977eb8b1db2fb57eb1ab0d90
EvenAsl <85791946+EvenAsl@users.noreply.github.com>
2022-07-05 18:06:23 +0200

    Invite gmail only

--- src/pages/restricted/invite.tsx
+++ src/pages/restricted/invite.tsx
@@ -16,6 +16,11 @@ const Invite: NextPage = () => {
 			<div className="p-2" />
 			<Formik
 				initialValues={{ email: "" }}
+				validate={(values) => {
+					if (!values.email.includes("@gmail.com")) {
+						return { email: "Must be a gmail!" };
+					}
+				}}
 				onSubmit={(values, { setErrors }) => {
 					invite.mutate({ email: values.email });
 					if (!invite.isSuccess && invite.isError) {
@@ -28,19 +33,36 @@ const Invite: NextPage = () => {
 				{() => (
 					<div className="flex flex-col items-center">
 						<Form className="w-full max-w-xs form-control">
-							<label className="label label-text">Email</label>
+							<label className="label label-text">Gmail</label>
 							<Field
 								className="input w-full max-w-xs"
 								type="email"
 								name="email"
-								placeholder="Email"
+								placeholder="Gmail"
 							/>
 							<ErrorMessage
 								name="email"
 								render={(msg) => (
 									<>
 										<div className="p-2" />
-										<div>{msg}</div>
+										<div className="alert alert-error shadow-lg">
+											<div>
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													className="stroke-current flex-shrink-0 h-6 w-6"
+													fill="none"
+													viewBox="0 0 24 24"
+												>
+													<path
+														strokeLinecap="round"
+														strokeLinejoin="round"
+														strokeWidth="2"
+														d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+													/>
+												</svg>
+												<span>Error! {msg}</span>
+											</div>
+										</div>
 									</>
 								)}
 							/>
