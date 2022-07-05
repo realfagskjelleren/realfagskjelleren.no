24f90d633194b0f5113907acbe27b6be02cb1768
EvenAsl <85791946+EvenAsl@users.noreply.github.com>
2022-07-05 15:01:51 +0200

    Only use google signin

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,5 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
@@ -10,24 +9,9 @@ export const authOptions: NextAuthOptions = {
 	// Configure one or more authentication providers
 	adapter: PrismaAdapter(prisma),
 	providers: [
-		GithubProvider({
-			clientId: process.env.GITHUB_ID,
-			clientSecret: process.env.GITHUB_SECRET,
-		}),
-		// ...add more providers here
-		CredentialsProvider({
-			name: "Credentials",
-			credentials: {
-				name: {
-					label: "Name",
-					type: "text",
-					placeholder: "Enter your name",
-				},
-			},
-			async authorize(credentials) {
-				const user = { id: 1, name: credentials?.name ?? "J Smith" };
-				return user;
-			},
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
 		}),
 	],
 };
