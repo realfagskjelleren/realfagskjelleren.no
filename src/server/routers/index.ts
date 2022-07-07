4b64513cf5d007b6918a8fd94be3a3521519f452
EvenAsl <85791946+EvenAsl@users.noreply.github.com>
2022-07-07 14:36:00 +0200

    User router, and query all

--- src/server/routers/index.ts
+++ src/server/routers/index.ts
@@ -4,11 +4,13 @@ import superjson from "superjson";
 import { authRouter } from "./auth";
 import { inviteRouter } from "./invite";
 import { goodRouter } from "./good";
+import { userRouter } from "./user";
 
 export const appRouter = createRouter()
 	.transformer(superjson)
 	.merge("auth.", authRouter)
 	.merge("invite.", inviteRouter)
+	.merge("user.", userRouter)
 	.merge("good.", goodRouter);
 
 // export type definition of API
